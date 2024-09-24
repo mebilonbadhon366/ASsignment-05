f9dd6b22b21e2f6249330a39bedb4e7d79e0b3b4
mebilonbadhon366 <mebilonhossainbd@gmail.com>
2024-09-25 02:29:02 +0600

    added all js and section

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
+ 
+//  donate for noakhali card function start here 
+ 
  const donateButton=document.getElementById("donate");
  donateButton.addEventListener('click',function() {
 
-    
     const donation = parseFloat(document.getElementById("donation").value);
-    
 
+    // if(donation <= 0 || isNaN(donation)){
+    //     document.getElementById("donation-error").classList.remove('hidden');
+    //     return ;
+        
+    //    };
    const totalDonation = parseFloat(document.getElementById('total-donation').innerText);
- // console.log(typeof totalDonation,typeof donation);
-
- // console.log( totalDonation);
+ 
   const newDonation= totalDonation + donation;
   const newDonationElement=document.getElementById('total-donation')
    newDonationElement.innerText=newDonation;
@@ -19,16 +23,13 @@
   const newBalanceElement=document.getElementById('balance');
   newBalanceElement.innerText=newBalance;
 
-
-
-
-
+// history tab information 
 
    const historyItem = document.createElement("div");
    historyItem.className='bg-white p-3 rounded border1-2 border-indigo-500';
 
     historyItem.innerHTML =
-    `<p>${donation} Taka is donated for famine-2024 at Feni, Bangladesh</p>
+    `<p>${donation} Taka is donated for famine-2024 at Noakhali, Bangladesh</p>
    <p>${new Date().toString()}</p>
 
             `; 
@@ -40,6 +41,118 @@
 
 
      });
+
+//  donate for noakhali card function end here 
+ 
+//  donate for feni card function start here 
+ 
+const donateNowButton =document.getElementById("Donate");
+donateNowButton.addEventListener('click',function() {
+
+   
+   const donation = parseFloat(document.getElementById("Donation").value);
+
+
+   // if(donation <= 0 || isNaN(donation)){
+   //     document.getElementById("donation-error").classList.remove('hidden');
+   //     return ;
+       
+   //    };
+
+   
+   
+  
+  const totalDonation = parseFloat(document.getElementById('Total-Donation').innerText);
+// console.log(typeof totalDonation,typeof donation);
+
+// console.log( totalDonation);
+
+ const newDonation= totalDonation + donation;
+ const newDonationElement=document.getElementById('Total-Donation')
+  newDonationElement.innerText=newDonation;
+
+ const balance= parseFloat(document.getElementById('balance').innerHTML);
+
+ const newBalance= balance - donation ;
+ const newBalanceElement=document.getElementById('balance');
+ newBalanceElement.innerText=newBalance;
+
+ 
+
+
+
+// history tab information 
+
+  const historyItem = document.createElement("div");
+  historyItem.className='bg-white p-3 rounded border1-2 border-indigo-500';
+
+   historyItem.innerHTML =
+   `<p>${donation} Taka is donated for famine-2024 at Feni, Bangladesh</p>
+  <p>${new Date().toString()}</p>
+
+           `; 
+
+  const historyContainer=document.getElementById("history-list");
+   historyContainer.insertBefore(historyItem,historyContainer.firstChild);
+
+    document.getElementById('history-list').classList.remove('hidden');
+
+
+    });
+
+//  donate for feni card function end here
+
+
+// /  donate for qoutamovement card function start here 
+ 
+ const qoutaDonateButton=document.getElementById("donation-amount");
+ qoutaDonateButton.addEventListener('click',function() {
+
+    const donation = parseFloat(document.getElementById("donation-given").value);
+
+    // if(donation <= 0 || isNaN(donation)){
+    //     document.getElementById("donation-error").classList.remove('hidden');
+    //     return ;
+        
+    //    };
+   const totalDonation = parseFloat(document.getElementById('total-donation-amount').innerText);
+ 
+  const newDonation= totalDonation + donation;
+  const newDonationElement=document.getElementById('total-donation')
+   newDonationElement.innerText=newDonation;
+
+  const balance= parseFloat(document.getElementById('balance').innerHTML);
+
+  const newBalance= balance - donation ;
+  const newBalanceElement=document.getElementById('balance');
+  newBalanceElement.innerText=newBalance;
+
+// history tab information 
+
+   const historyItem = document.createElement("div");
+   historyItem.className='bg-white p-3 rounded border1-2 border-indigo-500';
+
+    historyItem.innerHTML =
+    `<p>${donation} Taka is donated for famine-2024 at Quotamovement, Bangladesh</p>
+   <p>${new Date().toString()}</p>
+
+            `; 
+
+   const historyContainer=document.getElementById("history-list");
+    historyContainer.insertBefore(historyItem,historyContainer.firstChild);
+
+     document.getElementById('history-list').classList.remove('hidden');
+
+
+     });
+
+//  donate for quotamovement card function end here 
+
+
+
+
+
+
 
 
 
